18cbe20d56e21d57747c7ade354648b5d28b51ac
Amadi Someswaran <amadi@someswaran.com>
2020-11-03 15:16:38 -0500

    module 6: completed

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const app = express()
 const morgan = require("morgan")
 const bodyparser = require("body-parser")
 
+
 //morgan middleware
 app.use(morgan("dev"))
 
@@ -16,7 +17,20 @@ app.use(bodyparser.urlencoded({ extended: true}))
 
 app.use("/api", require("./api"))
 
+//serves index.html for an route besides /api
 app.use("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public/index.html" ))
 })
 
+//handles 500 server errors
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)//what's this?
+  res.status(err.status || 500).send(err.message || "Internal server error.")
+})
+
+const PORT = process.env.PORT || 1337
+
+app.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`)
+})
