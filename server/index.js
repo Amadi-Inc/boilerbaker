4f86d5d042b5141fbdafb9dc5fccbcaa6431bd15
Amadi Someswaran <amadi@someswaran.com>
2020-11-03 14:48:39 -0500

    module 6: send index html

--- server/index.js
+++ server/index.js
@@ -8,10 +8,15 @@ const bodyparser = require("body-parser")
 app.use(morgan("dev"))
 
 //static middleware
-app.use(express.static(path.join(__dirname, "../public"))) //not sure if correct
+app.use(express.static(path.join(__dirname, "..", "/public"))) //why do we use this syntax?
 
 //body parsing middleware
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true}))
 
-app.use("/api", require("./routes"))
+app.use("/api", require("./api"))
+
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html" ))
+})
+
